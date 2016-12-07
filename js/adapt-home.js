1d75e3ad4089fe6aeb837eb80a7c8deceac58e43
RobertPeek <robert@delta-net.co.uk>
2016-12-07 10:39:59 +0000

    Fixes #7
    Re-built extension inline with the Toolbox extension in terms of the way
    the extension is loaded as I think this was the issue in the authoring
    tool with the start routing.

--- js/adapt-home.js
+++ js/adapt-home.js
@@ -2,68 +2,30 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var Backbone = require('backbone');
+    var HomeView = require('extensions/adapt-home/js/adapt-homeView');
 
-    var HomeButton = Backbone.View.extend({
+    function setupHome(toolboxModel) {
 
-        className: 'home',
+        var homeModel = new Backbone.Model(homeModel);
 
-        initialize: function() {
-            this.listenTo(Adapt, 'remove', this.remove);
-            this.render();
-        },
+        Adapt.on('navigationView:postRender', function(navigationView) {
 
-        events: {
-            'click .home-button': 'initLink'
-        },
+          // Hide main navigation back button
+          $('.navigation-back-button').css('display','none');
 
-        render: function() {
-            var data = this.model.toJSON();
-            var template = Handlebars.templates["home"];
-            this.$el.html(template(data)).prependTo('#wrapper'+'>.navigation'+'>.navigation-inner');
+          navigationView.$('.navigation-drawer-toggle-button').before(new HomeView({
+            model: homeModel
+          }).$el);
 
-            // Hide main navigation back button
-            $('.navigation-back-button').css('display','none');
+        });
 
-            // Add icon to button
-            $('.home-button').addClass(Adapt.course.get('_home')._icon);
+    }
 
-            // Check for pages to hide on
-            this.hideButton();
-
-            return this;
-
-        },
-
-        hideButton: function() {
-
-          var hideIDs = Adapt.course.get('_home')._hide;
-
-          var hasHideIdsConfiguration = (hideIDs && hideIDs.length > 0);
-
-          if (hasHideIdsConfiguration) {
-            for (var i = 0, l =  hideIDs.length; i < l; i++) {
-              var item = hideIDs[i];
-              var id = item._id;
-
-              if(item._id === Adapt.location._currentId) {
-                $('.home-button').css('display','none');
-              }
-
-            }
-          }
-        },
-
-        initLink: function(event) {
-            if (event) event.preventDefault();
-            Adapt.navigateToElement('.' + Adapt.course.get('_home')._link);
-        }
-
-    });
-
-    Adapt.on('router:page', function(pageModel) {
+    Adapt.once('app:dataReady', function() {
 
         if(Adapt.course.get('_home')._isEnabled){
-            new HomeButton({model:pageModel});
+            var courseHomeConfig = Adapt.course.get('_home');
+            setupHome(courseHomeConfig, courseHomeConfig._items);
         }
 
     });
---
+++ js/adapt-homeView.js
@@ -0,0 +1,69 @@
+define(function(require) {
+
+    var Adapt = require('coreJS/adapt');
+    var Backbone = require('backbone');
+
+    var HomeView = Backbone.View.extend({
+
+        className: 'home',
+
+        initialize: function() {
+          this.render();
+          this.listenTo(Adapt, 'pageView:ready menuView:ready', this.onNavigationEnd);
+        },
+
+        events: {
+            'click .home-button': 'initLink'
+        },
+
+        render: function() {
+
+            var data = this.model.toJSON();
+            var template = Handlebars.templates['home'];
+            this.$el.html(template({
+                home:data
+            }));
+
+            // Add icon to button
+            this.$('.home-button').addClass(Adapt.course.get('_home')._icon);
+
+            // Check for pages to hide on
+            this.checkIDs();
+        },
+
+        checkIDs: function() {
+
+          // Show button
+          this.$('.home-button').css('display','block');
+
+          var hideIDs = Adapt.course.get('_home')._hide;
+
+          var hasHideIdsConfiguration = (hideIDs && hideIDs.length > 0);
+
+          if (hasHideIdsConfiguration) {
+            for (var i = 0, l =  hideIDs.length; i < l; i++) {
+              var item = hideIDs[i];
+              var id = item._id;
+
+              if(id == Adapt.location._currentId) {
+                this.$('.home-button').css('display','none');
+              }
+
+            }
+          }
+        },
+
+        initLink: function(event) {
+            if (event) event.preventDefault();
+            Adapt.navigateToElement('.' + Adapt.course.get('_home')._link);
+        },
+
+        onNavigationEnd: function(view) {
+            this.checkIDs();
+        }
+
+    });
+
+    return HomeView;
+
+});
